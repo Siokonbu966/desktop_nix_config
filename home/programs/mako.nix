e3423008536726a4ce890847d986de1a4c1a9911
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-04-10 22:15:05 +0900

    set config mako theme

---
+++ home/programs/mako.nix
@@ -0,0 +1,20 @@
+{ ... }:
+{
+  services.mako = {
+    enable = true;
+
+    backgroundColor = "#1e1e2e";
+    textColor = "#cdd6f4";
+    borderColor = "#89b4fa";
+
+    borderSize = 2;
+    defaultTimeout = 3000;
+
+    font = "Noto Sans 10";
+    
+    extraConfig = ''
+      [urgency=high]
+      border-color=#f38ba8
+    '';
+  };
+}
