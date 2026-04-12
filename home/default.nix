cf881667930ec037fef8382a9bb532887e9b4461
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-04-12 22:08:09 +0900

    setup default app for url

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
     ./programs/wm
     ./programs/mako.nix
     ./programs/zsh.nix
+    ./programs/default-app.nix
   ];
 
   home = rec {
--- home/programs/default-app.nix
+++ home/programs/default-app.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 {
-  edg.mimeApps = {
+  xdg.mimeApps = {
     enable = true;
 
     defaultApplications = {
       "x-scheme-handler/http" = ["firefox.desktop"];
       "x-scheme-handler/https" = ["firefox.desktop"];
       "text/html" = ["firefox.desktop"];
-    }
-  }
+    };
+  };
 }
