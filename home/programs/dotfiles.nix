a374917910ee18ea7f57d958f296709ab3b2a646
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-05-05 23:40:44 +0900

    test niri config2

--- home/programs/dotfiles.nix
+++ home/programs/dotfiles.nix
@@ -6,7 +6,7 @@
     "kitty".source = "${my-dotfiles}/kitty";
     "fastfetch".source = "${my-dotfiles}/fastfetch";
     "picom".source = "${my-dotfiles}/picom.conf";
-    "niri".source = "../../configs/niri";
+    "niri".source = ../../configs/niri;
   };
   home.file = {
     ".p10k.zsh".source = "${my-dotfiles}/.p10k.zsh";
