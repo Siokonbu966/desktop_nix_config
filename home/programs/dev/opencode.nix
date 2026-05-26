f3791a47e0390fd02efc9ed7d1bbf6f3d914c4ac
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-05-26 11:11:42 +0900

    configure opencode using home-manager

--- home/programs/dev/opencode.nix
+++ home/programs/dev/opencode.nix
@@ -1,20 +1,11 @@
 {...}:
 {
-  progrmas.opencode = {
+  programs.opencode = {
     enable = true;
     settings = {
+      theme = "kanagawa";
       permission = {
         websearch = "allow";
-      };
-      theme = "kanagawa";
-      mcp = {
-        context7 = {
-          type = "remote";
-          url = "https://mcp.context7.com/mcp";
-          headers = {
-            CONTEXT7_API_KEY = "$CONTEXT7_API_KEY";
-          };
-        };
       };
     };
   };
