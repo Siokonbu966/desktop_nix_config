ed478bf7c3297b10349c166610901f9cf8ac771b
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-06-07 19:41:24 +0900

    ghostty: change font size for surface

--- home/programs/gui/ghostty.nix
+++ home/programs/gui/ghostty.nix
@@ -1,24 +1,27 @@
 { device ? "desktop", ...}:
 let
   deviceConfig = if device == "desktop" then ''
+    font-size = 17
     window-decoration = none
     # background-image = /home/crocus/Pictures/Wallpapers/mahuyu_26_birthday.jpg
     # background-image-fit = cover
     # background-image-opacity = 0.1
     background-opacity = 0.7
   '' else if device == "mac" then ''
+    font-size = 17
     macos-titlebar-style = hidden
     background-image = /Users/crocus/Pictures/Wallpapers/cho-kaguya_concept01c.png
     background-image-fit = cover
     background-image-opacity = 0.1
     # background-opacity = 0.7
+  '' else if device == "surface" then ''
+    font-size = 12
   '' else '''';
 in
 {
   xdg.configFile."ghostty/config".text = ''
     font-family = Mononoki Nerd Font
     font-family = Kosugi Maru
-    font-size = 17
 
     working-directory = home
     window-inherit-working-directory = false
