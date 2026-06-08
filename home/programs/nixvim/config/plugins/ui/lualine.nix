f82cf6c5cdcb0a83ab4e574a1e8f10dd3482f77e
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-06-08 14:32:47 +0900

    nixvim: change separator icon

--- home/programs/nixvim/config/plugins/ui/lualine.nix
+++ home/programs/nixvim/config/plugins/ui/lualine.nix
@@ -8,7 +8,7 @@
         icons_enabled = true;
         theme = "auto";
         component_separators = { left = ""; right = ""; };
-        section_separators = { left = ""; right = ""; };
+        section_separators = { left = ""; right = ""; };
         disabled_filetypes = {
           statusline = [ ];
           winbar = [ ];
