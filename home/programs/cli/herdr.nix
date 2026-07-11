17624d069f6ea057ed5a393c4d6c2ca9d3e19bbf
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-07-11 22:31:16 +0900

    refactor: move herdr config to xdg.configFile

--- home/programs/cli/herdr.nix
+++ home/programs/cli/herdr.nix
@@ -1,27 +1,30 @@
 { pkgs, inputs, ... }:
 {
-  programs.herdr = {
-    enable = true;
-    package = inputs.herdr.packages.${pkgs.system}.default;
-    settings = {
-      onboarding = false;
-      terminal = {
-        default_shell = "nu";
-        new_cwd = "follow";
-        shell_mode = "auto";
+  home.packages = [
+    inputs.herdr.packages.${pkgs.system}.default
+  ];
+
+  xdg.configFile."herdr/config.toml".source = (pkgs.formats.toml { }).generate "config.toml" {
+    onboarding = false;
+    terminal = {
+      default_shell = "nu";
+      new_cwd = "follow";
+      shell_mode = "auto";
+    };
+    theme = {
+      auto_switch = true;
+      name = "catppuccin";
+      dark_name = "catppuccin";
+      light_name = "catppuccin-latte";
+    };
+    ui = {
+      sidebar_width = 32;
+      sound = {
+        enabled = true;
       };
-      theme = {
-        auto_switch = true;
-        name = "catppuccin";
-        dark_name = "catppuccin";
-        light_name = "catppuccin-latte";
-      };
-      ui = {
-        sidebar_width = 32;
-        sound = {
-          enabled = true;
-        };
-      };
+    };
+    keys = {
+      prefix = "ctrl+g";
     };
   };
 }
