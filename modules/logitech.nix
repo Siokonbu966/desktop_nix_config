39b5fee3b2968c24536a7f98a1c8b4ba788cbc59
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-07-15 17:49:08 +0900

    logi: change solaar to logiops

--- modules/logitech.nix
+++ modules/logitech.nix
@@ -3,6 +3,23 @@
   hardware.logitech.wireless.enable = true;
 
   environment.systemPackages = [
-    pkgs.solaar
+    # pkgs.solaar
   ];
+
+  services.logiops = {
+    enable = true;
+    config = {
+      devices = [
+        {
+          name = "G304 Lightspeed Wireless Gaming Mouse";
+          dpi = 600;
+          hiresscroll = {
+            hires = true;
+            invert = false;
+            target = true;
+          };
+        }
+      ];
+    };
+  };
 }
